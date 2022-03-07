8654e21096cb9d9a61265904eb49d16cc245ce07
hyhwa414 <hyhwa414@gmail.com>
2022-03-07 23:28:53 +0900

    2022.03.07 acmicpc 2525

---
+++ swift/acmicpc/acmicpc2525/acmicpc2525/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  acmicpc2525
+//
+//  Created by 정현화 on 2022/03/07.
+//  Copyright © 2022 com.hyhwa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
