ccb86b1c9981aeb09917e81c8e6f94a2d882eae4
hyhwa414 <hyhwa414@gmail.com>
2022-03-16 15:09:16 +0900

    2022.03.16 acmicpc 2480

--- swift/acmicpc/acmicpc2525/acmicpc2525/main.swift
+++ swift/acmicpc/acmicpc2525/acmicpc2525/main.swift
@@ -6,7 +6,22 @@
 //  Copyright © 2022 com.hyhwa. All rights reserved.
 //
 
+// 2022.03.07 acmicpc 2525 - 오븐 시계
+
 import Foundation
 
-print("Hello, World!")
+let input1 = readLine()!.components(separatedBy: " ")
+let hour = Int(input1[0])!
+let minute = Int(input1[1])!
 
+let input2 = readLine()!
+let time = Int(input2)!
+
+var nextMinute = minute + time
+var nextHour = (nextMinute/60) + hour
+
+if nextHour > 23 {
+    nextHour = nextHour % 24
+}
+
+print("\(nextHour) \(nextMinute%60)")
